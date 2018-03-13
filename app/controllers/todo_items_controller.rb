d83cdaa7fac54b4e8d5afa2a8563f3408ee37cce
chris <chris@debian>
2018-03-13 14:56:36 -0400

    Added completion marks and methods

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,6 +1,7 @@
 class TodoItemsController < ApplicationController
 
 	before_action :set_todo_list
+	before_action :set_todo_item, except: [:create]
 
 	def create
 		@todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -10,8 +11,7 @@ class TodoItemsController < ApplicationController
 	end
 
 	def destroy
-
-		@todo_item = @todo_list.todo_items.find(params[:id])
+		
 		if @todo_item.destroy
 			flash[:success] = "Item Deleted"
 		else
@@ -21,12 +21,25 @@ class TodoItemsController < ApplicationController
 		redirect_to todo_list_path(@todo_list)
 	end
 
+	def complete
+
+		@todo_item.update_attribute(:completed_at, Time.now)
+		redirect_to todo_list_path(@todo_list), notice: "Todo Item Completed"
+
+
+	end
+
+
 
 
 	private
 
 	def set_todo_list
 		@todo_list = TodoList.find(params[:todo_list_id])
+	end
+
+	def set_todo_item
+		@todo_item = @todo_list.todo_items.find(params[:id])
 	end
 
 	def todo_item_params
